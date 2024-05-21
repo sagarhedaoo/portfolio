0132a5e4ddfe8d74c14c88d3c5b3b5f29603943d
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-05-21 01:02:47 -0400

    Fixed Sentry bugs

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,7 +3,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://94dcc0b7995ad36e93cd9bbab24fd441@o4507290111180800.ingest.us.sentry.io/4507290113343488",
@@ -13,12 +12,6 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
-  integrations: [
-    // Add profiling integration to list of integrations
-    nodeProfilingIntegration(),
-  ],
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
