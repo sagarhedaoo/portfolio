16e2ca980811521012c56145d96dbe145395bc7a
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-05-28 13:02:07 -0400

    Updates

--- data/index.ts
+++ data/index.ts
@@ -39,7 +39,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "F1 OPT",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
