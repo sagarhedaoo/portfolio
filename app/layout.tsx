ce850ae336299c3e1fa4e206e49bff5d5ef65de7
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-05-28 19:17:01 -0400

    Metadata Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,17 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sagar's Portfolio",
+  description: "Portfolio",
+  openGraph: {
+    title: "Sagar's Portfolio",
+    description: "Portfolio",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,25 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>Sagar&apos;s Portfolio</title>
-        <meta name="description" content="Portfolio" />
-        <meta property="og:title" content="Sagar Hedaoo - Portfolio" />
-        <meta
-          property="og:description"
-          content="Welcome to My portfolio. Explore my projects, skills, and experiences."
-        />
-        <meta
-          property="og:image"
-          content="https://sagarhedaoo.com/sagar-image.jpg"
-        />
-        <meta property="og:url" content="Sagar Hedaoo Portfolio" />
-        <meta property="og:site_name" content="Sagar Hedaoo - Portfolio" />
-        <meta
-          name="twitter:image:alt"
-          content="Sagar Hedaoo's Portfolio Thumbnail"
-        />
-      </Head>
       <body className={inter.className}>
         <GoogleAnalytics gaId="G-670NTESQHE" />
         <ThemeProvider
